f4af0667df3a36861b0500a917eb2d02e4483178
lixinyuan1216 <lixinyuan1216@gmail.com>
2016-06-02 19:55:14 +1000

    completed project
    -----------------
    
    bux fixed

--- src/test/java/com/data/component/JsonParserTest.java
+++ src/test/java/com/data/component/JsonParserTest.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 /**
- * Created by xy on 10/05/16.
- */
-public class JsonParserTest {
-    JsonParser jp;
+     * Created by xy on 10/05/16.
+     */
+    public class JsonParserTest {
+        JsonParser jp;
 
-   @Test
-    public void testReader() throws Exception {
-        final String path = "src/test/testData/Rndm5ptsProjected.json";
+        @Test
+        public void testReader() throws Exception {
+            final String path = "src/test/testData/Rndm5ptsProjected.json";
 
-        jp = new JsonParser();
-        jp.readJSONFIle(path);
+            jp = new JsonParser();
+            jp.readJSONFIle(path);
 
-        int size = jp.getSource().getFeatures().size();
-        assertEquals(10, size);
+            int size = jp.getSource().getFeatures().size();
+            assertEquals(10, size);
 
-    }
+        }
 
     /*@Test
     public void testWriter() throws Exception {
